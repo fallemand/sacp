dedc51a1a121e55fe556d8110656ea59187737d6
Facundo Allemand <facundo.allemand@mercadolibre.com>
2016-08-08 15:59:37 -0300

    Widgets home.

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,16 +1,27 @@
 'use strict';
-(function(){
+(function () {
 
-class HomeComponent {
-  constructor() {
-    this.message = 'Hello';
-  }
-}
+    class HomeComponent {
+        constructor($http) {
+            $http.get('/api/users/count?active=false&&role=user').then(response => {
+                this.countNotActiveUsers = response.data;
+            });
 
-angular.module('sacpApp')
-  .component('home', {
-    templateUrl: 'app/home/home.html',
-    controller: HomeComponent
-  });
+            $http.get('/api/treatments?state=EA').then(response => {
+                this.treatmentsEA = response.data.total;
+            });
+
+            $http.get('/api/treatments?state=PA').then(response => {
+                this.treatmentsPA = response.data.total;
+            });
+        }
+    }
+
+    angular.module('sacpApp')
+        .component('home', {
+            templateUrl: 'app/home/home.html',
+            controller: HomeComponent,
+            controllerAs: 'vm'
+        });
 
 })();
