7cd42cbe8974b4c0a18fe6b02e43ea201c0fd35d
Facundo Allemand <facundo.allemand@mercadolibre.com>
2016-09-24 17:14:50 -0300

    fix prescription pdf

--- server/api/report/report.controller.js
+++ server/api/report/report.controller.js
@@ -14,38 +14,46 @@ import Treatment from '../treatment/treatment.model';
 
 // Gets a single Report from the DB
 export function getReport(req, res) {
-    Treatment.findById(req.params.id, function (err, object) {
-        if (err || !object) {
-            var error = err || 'Verifique los parámetros';
-            return res.status(500).send(error);
-        }
-        var fs = require('fs');
-        var pdf = require('html-pdf');
-        var template = fs.readFileSync('./client/reports/prescription.html', 'utf8');
-        var serverUrl = req.protocol + '://' + req.get('host');
-        object.serverUrl = serverUrl;
+    Treatment.findById(req.params.id)
+        .populate('doctor')
+        .populate('patient')
+        .populate({
+            path: 'patient',
+            populate: { path: 'agreementType' }
+        })
+        .populate('drugs.type')
+        .exec(function (err, object) {
+            if (err || !object) {
+                var error = err || 'Verifique los parámetros';
+                return res.status(500).send(error);
+            }
+            var fs = require('fs');
+            var pdf = require('html-pdf');
+            var template = fs.readFileSync('./client/reports/prescription.html', 'utf8');
+            var serverUrl = req.protocol + '://' + req.get('host');
+            object.serverUrl = serverUrl;
 
-        var Handlebars = require('handlebars');
-        template = Handlebars.compile(template);
+            var Handlebars = require('handlebars');
+            template = Handlebars.compile(template);
 
-        Handlebars.registerHelper('formatDate', function(date) {
-            return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
-        });
-        var html = template(object);
-
-        console.log(serverUrl);
-
-
-        var options = { format: 'A4' };
-        pdf.create(html, options).toStream(function(err, stream) {
-            res.set({
-                "Content-Disposition":"attachment; filename=" + req.params.report + "-" + req.params.id + ".pdf",
-                "Content-Type": 'application/pdf',
-                'Access-Control-Allow-Origin': '*'
+            Handlebars.registerHelper('formatDate', function(date) {
+                return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
             });
-            res.status(200);
-            stream.pipe(res);
+            var html = template(object);
 
-        });
+
+
+
+            var options = { format: 'A4' };
+            pdf.create(html, options).toStream(function(err, stream) {
+                res.set({
+                    "Content-Disposition":"attachment; filename=" + req.params.report + "-" + req.params.id + ".pdf",
+                    "Content-Type": 'application/pdf',
+                    'Access-Control-Allow-Origin': '*'
+                });
+                res.status(200);
+                stream.pipe(res);
+
+            });
     });
 }
